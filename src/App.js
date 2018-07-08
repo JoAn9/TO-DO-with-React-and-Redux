62b55c800b87f52df687e66b347ef547e12499ef
JoAn <joannacalinska@gmail.com>
2018-07-08 21:36:04 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,70 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import ToDoList from "./components/ToDoList";
+import Congratulations from "./components/Congratulations";
 
-const BasicExample = () => (
+const App = () => (
   <Router>
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Welcome</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">TO DO List</Link>
         </li>
         <li>
-          <Link to="/topics">Topics</Link>
+          <Link to="/topics">Congratulations</Link>
         </li>
       </ul>
 
       <hr />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route exact path="/" component={Welcome} />
+      <Route path="/about" component={ToDoList} />
+      <Route path="/topics" component={Congratulations} />
     </div>
   </Router>
 );
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
-
-export default BasicExample;
+export default App;
---
+++ src/components/ToDoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ToDoList = () => (
+  <div>
+    <h2>To Do</h2>
+  </div>
+);
+
+export default ToDoList;
