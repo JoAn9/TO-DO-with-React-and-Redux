96f11a7def12148b385f7b86496b07e8aef6cfdb
JoAn <joannacalinska@gmail.com>
2018-07-12 21:43:21 +0200

    style

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Congratulations from "./components/Congratulations";
 
 const App = () => (
   <Router>
-    <div style={{textAlign: 'center'}}>
-      <ul>
+    <div>
+      <ul style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
         <Link to="/"><Button bsStyle="info" style={{marginRight: '1em'}}>Welcome</Button></Link>
         <Link to="/list"><Button bsStyle="info" style={{marginRight: '1em'}}>TO DO List</Button></Link>
         <Link to="/congratulations"><Button bsStyle="info" disabled style={{marginRight: '1em'}}>Congratulations</Button></Link>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,56 @@
 import React from "react";
 import imageBackground from '../images/235.jpg';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
 
 const myStyle = {
   backgroundImage: "url("+imageBackground+")",
   backgroundSize: 'cover',
-  height: '100vh'
+  height: '90vh',
+  display: 'flex',
+  alignContent: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
 }
 
-const Welcome = () => (
-  <div style={myStyle}>
-    <h2>Welcome</h2>
-  </div>
-);
+const alignCenter = {
+  textAlign: 'center'
+}
+
+class Welcome extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = stateName => event => {
+    this.setState({
+      [stateName]: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div style={myStyle}>
+        <h2 style={alignCenter}>Hello {this.state.name}</h2>
+        <form style={alignCenter}>
+          <FormGroup
+            controlId="formBasicText"
+            style={{display: 'flex', justifyContent: 'center'}}
+            // validationState={this.getValidationState()}
+          >
+            <FormControl
+              type="text"
+              value={this.state.name}
+              placeholder="Your Name"
+              onChange={this.handleChange('name')}
+              style={alignCenter}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Welcome;
