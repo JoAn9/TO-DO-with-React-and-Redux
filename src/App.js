7e147605883f12afa0450f09e0a8afc9c0179a83
JoAn <joannacalinska@gmail.com>
2018-07-13 21:20:12 +0200

    styling styling not completed

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ import Congratulations from "./components/Congratulations";
 const App = () => (
   <Router>
     <div>
-      <ul style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-        <Link to="/"><Button bsStyle="info" style={{marginRight: '1em'}}>Welcome</Button></Link>
-        <Link to="/list"><Button bsStyle="info" style={{marginRight: '1em'}}>TO DO List</Button></Link>
-        <Link to="/congratulations"><Button bsStyle="info" disabled style={{marginRight: '1em'}}>Congratulations</Button></Link>
-      </ul>
-
       <Route exact path="/" component={Welcome} />
       <Route path="/list" component={ToDoList} />
       <Route path="/congratulations" component={Congratulations} />
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,28 +1,32 @@
 import React from "react";
-import imageBackground from '../images/235.jpg';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import imageBackground from '../images/235.jpg';
+import ToDoList from "./ToDoList";
+
 
 
 const myStyle = {
   backgroundImage: "url("+imageBackground+")",
   backgroundSize: 'cover',
-  height: '90vh',
+  height: '100vh',
   display: 'flex',
-  alignContent: 'center',
+  alignContent: 'spaceBetween',
   justifyContent: 'center',
   flexDirection: 'column',
-}
 
-const alignCenter = {
-  textAlign: 'center'
+
 }
 
 class Welcome extends React.Component {
   state = {
     name: '',
+    band: '',
   }
 
   handleChange = stateName => event => {
+    console.log(event);
     this.setState({
       [stateName]: event.target.value,
     })
@@ -31,11 +35,10 @@ class Welcome extends React.Component {
   render() {
     return (
       <div style={myStyle}>
-        <h2 style={alignCenter}>Hello {this.state.name}</h2>
-        <form style={alignCenter}>
+        <h2 style={{flexGrow: 1, textAlign: 'center', marginTop: 120}}>Hello {this.state.name}</h2>
+        <form style={{flexGrow: 1, display: 'flex', justifyContent: 'center'}}>
           <FormGroup
             controlId="formBasicText"
-            style={{display: 'flex', justifyContent: 'center'}}
             // validationState={this.getValidationState()}
           >
             <FormControl
@@ -43,11 +46,17 @@ class Welcome extends React.Component {
               value={this.state.name}
               placeholder="Your Name"
               onChange={this.handleChange('name')}
-              style={alignCenter}
             />
             <FormControl.Feedback />
+            <FormControl
+              type="text"
+              value={this.state.band}
+              placeholder="Your Favourite Band"
+              onChange={this.handleChange('band')}
+            />
           </FormGroup>
         </form>
+        <Button bsStyle="info" style={{width: 200, marginBottom: 20, textAlign: 'center'}}><Link to="/list">Next</Link></Button>
       </div>
     )
   }
