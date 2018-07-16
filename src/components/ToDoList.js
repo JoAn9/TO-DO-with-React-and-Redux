4ac71fb743498482a7b1a4081e0808a5dde91190
JoAn <joannacalinska@gmail.com>
2018-07-16 21:11:22 +0200

    installing material-ui & grid in to do list

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,9 +1,42 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const ToDoList = () => (
-  <div>
-    <h2>To Do</h2>
-  </div>
-);
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+
+class ToDoList extends React.Component {
+  render() {
+
+    // const {classes} = this.props;
+
+    return (
+      <div>
+        <Grid container spacing={24}>
+          <Grid item xs={12} md={4}>
+            <Paper>xs=6 sm=3</Paper>
+          </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+  </Grid>
+</div>
+    );
+  }
+}
 
 export default ToDoList;
