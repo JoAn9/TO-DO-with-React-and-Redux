8cbc490b6eb77825592e820c632653d8eb36e39e
JoAn <joannacalinska@gmail.com>
2018-09-02 21:59:23 +0200

    congratulations-header

--- src/components/Congratulations.js
+++ src/components/Congratulations.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import imageBackground from '../images/congratulation.jpg';
 
@@ -14,14 +15,21 @@ const styles = theme => ({
     backgroundSize: 'cover',
     height: '100vh',
   },
+  header: {
+    color: '#fff',
+    padding: 50,
+  },
 });
 
 class Congratulations extends React.Component {
   render() {
-    const { classes } = this.props;
+    const { classes, userFromRedux } = this.props;
+    console.log(userFromRedux.user.name);
     return (
       <div className={classes.container}>
-        <h2>Congratulations</h2>
+        <h2 className={classes.header}>
+          Congratulations {userFromRedux.user.name}
+        </h2>
         <Button variant="raised" color="primary">
           <Link to="/list" style={{color: '#fff', fontWeight: 'bold'}}>Back to Tasks To Do</Link>
         </Button>
@@ -31,4 +39,11 @@ class Congratulations extends React.Component {
 };
 
 
-export default withStyles(styles)(Congratulations);
+function mapStateToProps(store) {
+  return {
+    userFromRedux: store.user,
+  }
+}
+
+export default connect(mapStateToProps,
+  null)(withStyles(styles)(Congratulations));
