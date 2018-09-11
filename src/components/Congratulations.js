386d3430e9c3d1d42877e3f55adc5d09e5f45990
JoAn <joannacalinska@gmail.com>
2018-09-11 21:55:38 +0200

    frame with video by axios

--- src/components/Congratulations.js
+++ src/components/Congratulations.js
@@ -1,29 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import imageBackground from '../images/congratulation.jpg';
 import YoutubeFrame from './YoutubeFrame';
 
 
 // dodac cancel axios
 // dodac toastr i errors handling
 
-const styles = theme => ({
-  container: {
-    backgroundImage: 'url(' + imageBackground + ')',
-    backgroundSize: 'cover',
-    height: '100vh',
-  },
-  header: {
-    color: '#fff',
-    padding: 50,
-  },
-});
 
 class Congratulations extends React.Component {
+  state = {
+    video: '',
+  }
 
   componentDidMount() {
     this.getSong();
@@ -33,33 +21,23 @@ class Congratulations extends React.Component {
     let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.props.userFromRedux.user.band}&type=video&part=snippet,contentDetails,statistics,statu&key=AIzaSyDCaZeK9ihaPCkzDDtKaAkGwVl_Pq5LktA`;
       
     axios.get(url)
-      .then(response => console.log(response))
-
-
-    //             this.setState({
-    //                 video: data.items[0].id.videoId,
-    //             })
-    //         });
+      .then(response => {
+        console.log(response);
+        const randomNmb1to4 = Math.floor(Math.random() * 4) + 1;
+        this.setState({
+          video: response.data.items[randomNmb1to4].id.videoId,
+        });
+      })
   }
 
   render() {
-    const { classes, userFromRedux } = this.props;
-
+    const { userFromRedux } = this.props;
+    const { video } = this.state;
     return (
-      <div className={classes.container}>
-        <h2 className={classes.header}>
-          Congratulations {userFromRedux.user.name}
-        </h2>
-        <YoutubeFrame />
-        <Button variant="raised" color="primary">
-          <Link to="/list" style={{color: '#fff', fontWeight: 'bold'}}>Back to Tasks To Do</Link>
-        </Button>
-      </div>
+      <YoutubeFrame video={video} user={userFromRedux} />
     )
   }
-
 }
-
 
 function mapStateToProps(store) {
   return {
@@ -68,15 +46,4 @@ function mapStateToProps(store) {
 }
 
 export default connect(mapStateToProps,
-  null)(withStyles(styles)(Congratulations));
-
-//   
-
-// render() {
-//     return (
-//         <div style={{display: 'flex', justifyContent: 'center', paddingTop: '10vh'}}>
-//             <iframe width="611" height="344" src={`https://www.youtube.com/embed/${this.state.video}`} frameBorder="0" allowFullScreen={true}></iframe>
-//         </div>
-//     );
-// }
-// }
+  null)(Congratulations);
--- src/components/YoutubeFrame.js
+++ src/components/YoutubeFrame.js
@@ -1,15 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import imageBackground from '../images/congratulation.jpg';
+
+
+
+const styles = theme => ({
+  container: {
+    backgroundImage: 'url(' + imageBackground + ')',
+    backgroundSize: 'cover',
+    height: '100vh',
+  },
+  header: {
+    color: '#fff',
+    padding: 50,
+  },
+});
+
 
 class YoutubeFrame extends React.Component {
   render() {
+    const { classes, user } = this.props;
     return (
-      <div>
-        <h2>
-          tu bedzie ramka
-        </h2>
-      </div>
+      <Grid container direction="column" alignItems="center" className={classes.container}>
+        <Grid item>
+          <h2 className={classes.header}>
+            Congratulations {user.user.name}
+          </h2>
+        </Grid>
+        <Grid item>
+          <h3 className={classes.header}>
+            Good job! You have completed very difficult task!
+          </h3>
+        </Grid>
+        <Grid item>
+          <iframe width="611" height="344" src={`https://www.youtube.com/embed/${this.props.video}`} frameBorder="0" allowFullScreen={true}></iframe>
+        </Grid>
+        <Grid item className={classes.header}>
+          <Button variant="raised" color="primary">
+            <Link to="/list" style={{color: '#fff', fontWeight: 'bold'}}>Back to Tasks To Do</Link>
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default YoutubeFrame;
+export default withStyles(styles)(YoutubeFrame);
